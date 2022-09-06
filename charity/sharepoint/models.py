b70812b6127d9ba62b4e938a148a9d596af23bff
Grzegorz Piędziejewski <grzegorz.piedziejewski@gmail.com>
2022-09-06 19:12:53 +0200

    models

--- charity/sharepoint/models.py
+++ charity/sharepoint/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64)
+
+TYPE = (
+    (1, "Foundation"),
+    (2, "Non governmental organisation"),
+    (3, "Local Fundraise"),
+)
+class Institution(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    type = models.IntegerField(choices=TYPE, default=1)
+    categories = models.ManyToManyField(Category)
+
+class Donation(models.Model):
+    id = models.AutoField(primary_key=True)
+    quantitiy = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=256)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=64)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField(max_length=1000)
+    user = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
+
+
